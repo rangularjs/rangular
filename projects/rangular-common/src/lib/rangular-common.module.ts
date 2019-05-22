3d462faf24fbb93e46e80696d828a3ce5ded0fe3
Mohammad Kazem Zaker <mk.zaker@gmail.com>
2019-05-22 12:21:20 +0430

    Fixed EnglishNumber directive.

--- projects/rangular-common/src/lib/rangular-common.module.ts
+++ projects/rangular-common/src/lib/rangular-common.module.ts
@@ -2,12 +2,12 @@ import {NgModule} from '@angular/core';
 import {RequireAuthorityDirective} from './directives/require-authority.directive';
 import {CommonModule} from '@angular/common';
 import {OnlyNumberDirective} from './directives/only-number.directive';
-import { EnglishNumberDirective } from './directives/english-number.directive';
+import {EnglishNumberDirective} from './directives/english-number.directive';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [RequireAuthorityDirective, OnlyNumberDirective, EnglishNumberDirective],
-  exports: [RequireAuthorityDirective, OnlyNumberDirective]
+  exports: [RequireAuthorityDirective, OnlyNumberDirective, EnglishNumberDirective]
 })
 export class RangularCommonModule {
 }
