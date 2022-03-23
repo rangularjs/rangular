d0a0f32630d4d08681a3ee6530d41c34558903d7
Mohammad Kazem Zaker <mk.zaker@gmail.com>
2022-03-23 20:56:10 +0430

    Fix date range validator.

--- projects/rangular-common/src/lib/common/validators.ts
+++ projects/rangular-common/src/lib/common/validators.ts
@@ -1,6 +1,6 @@
 import {FormGroup, ValidationErrors} from '@angular/forms';
 import {isNullOrUndefinedOrEmpty} from './utils';
-import {DATE_FORMAT, J_DATE_FORMAT} from './constants';
+import {J_DATE_FORMAT} from './constants';
 import moment from 'moment-jalaali';
 
 export function dateRange(fromDateField = 'fromDate', toDateField = 'toDate') {
@@ -8,9 +8,9 @@ export function dateRange(fromDateField = 'fromDate', toDateField = 'toDate') {
     const td = control.get(toDateField).value;
     const fd = control.get(fromDateField).value;
     if (!isNullOrUndefinedOrEmpty(td) && !isNullOrUndefinedOrEmpty(fd)) {
-      const toDate = moment(td, J_DATE_FORMAT).format(DATE_FORMAT);
-      const fromDate = moment(fd, J_DATE_FORMAT).format(DATE_FORMAT);
-      return toDate && fromDate && fromDate > toDate ? {range: true} : null;
+      const toDate = moment(td, J_DATE_FORMAT);
+      const fromDate = moment(fd, J_DATE_FORMAT);
+      return toDate.isValid() && fromDate.isValid() && fromDate.isAfter(toDate) ? {range: true} : null;
     }
     return null;
   };
